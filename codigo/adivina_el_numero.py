dcf0d4095cb513955c518b540f4bfd33a3b1df4a
Javier Garcia-Algarra <jgalgarra@coit.es>
2018-01-19 16:11:21 +0100

    rango del numero secreto en el programa adivina

--- codigo/adivina_el_numero.py
+++ codigo/adivina_el_numero.py
@@ -8,7 +8,7 @@ import random                 # Este paquete es necesario para generar números 
 print("Hola, vamos a jugar a adivinar un número secreto entre 1 y 100")
 
 # Elegir al azar un número entero entre 1 y 100
-numero_secreto = random.randrange(1,11)
+numero_secreto = random.randrange(1,101)
 
 print("Ya lo he pensado, podemos empezar")
 intentos = 1                  # Contador de intentos. Empieza en uno
